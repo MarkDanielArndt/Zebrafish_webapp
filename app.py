fdc601dde45666f2e6a28438403ad3f3dd72ca51
markdanielarndt <markdaniel.arndt@gmail.com>
2026-01-10 17:10:40 +0100

    Update files

--- app.py
+++ app.py
@@ -357,5 +357,5 @@ with gr.Blocks() as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860, share=False)
+    demo.launch(server_name="0.0.0.0", server_port=7860, share=True)
 
